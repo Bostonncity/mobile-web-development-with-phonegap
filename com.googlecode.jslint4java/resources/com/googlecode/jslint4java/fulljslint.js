5689d1a5d55fa0a7750146e8fa7873a7c2e75b61
paul.beusterien <paul.beusterien@gmail.com>
2011-02-19 04:26:32 +0000

    Back up to r38 to prepare for jslint-track branch

--- com.googlecode.jslint4java/resources/com/googlecode/jslint4java/fulljslint.js
+++ com.googlecode.jslint4java/resources/com/googlecode/jslint4java/fulljslint.js
@@ -368,13 +368,10 @@ var JSLINT = (function () {
             devel      : true, // if logging should be allowed (console, alert, etc.)
             es5        : true, // if ES5 syntax should be allowed
             evil       : true, // if eval should be allowed
-            eqeqeq     : true, // if === should be required
             forin      : true, // if for in statements must filter
             fragment   : true, // if HTML fragments should be allowed
-            needcurly  : true, // if block is required with for and while statements
             newcap     : true, // if constructor names must be capitalized
             nomen      : true, // if names should be checked
-            nonew      : true, // if using `new` for side-effects should be disallowed
             on         : true, // if HTML event handlers should be allowed
             onevar     : true, // if only one var statement per function should be allowed
             passfail   : true, // if the scan should stop on first error
@@ -384,10 +381,8 @@ var JSLINT = (function () {
             undef      : true, // if variables should be declared before used
             safe       : true, // if use of some browser features should be restricted
             windows    : true, // if MS Windows-specigic globals should be predefined
-            semireq    : true, // if semicolons are required
             strict     : true, // require the "use strict"; pragma
             sub        : true, // if all forms of subscript notation are tolerated
-            varsattop  : true, // if all var declaration should be at top of functions
             white      : true, // if strict whitespace rules apply
             widget     : true  // if the Yahoo Widgets globals should be predefined
         },
@@ -2590,9 +2585,7 @@ loop:   for (;;) {
 
     function semicolon() {
         if (nexttoken.id !== ';') {
-            if (option.semireq) {
-                warn(bundle.expected_a_b, nexttoken, ';', nexttoken.value);
-            }
+            warn(bundle.expected_a_b, nexttoken, ';', nexttoken.value);
         } else {
             if (option.white) {
                 no_space_only();
@@ -2921,7 +2914,7 @@ loop:   for (;;) {
     function relation(s, eqeq) {
         var x = infix(s, 100, function (left, that) {
             check_relation(left);
-            if (option.eqeqeq && eqeq) {
+            if (eqeq) {
                 warn(bundle.expected_a_b, that, eqeq, that.id);
             }
             var right = expression(100);
@@ -3106,7 +3099,7 @@ loop:   for (;;) {
 // assignment or invocation or delete.
 
                 if (the_statement.id === '(') {
-                    if (option.nonew && the_statement.first.id === 'new') {
+                    if (the_statement.first.id === 'new') {
                         warn(bundle.bad_new);
                     }
                 } else if (!the_statement.assign &&
@@ -3243,9 +3236,7 @@ loop:   for (;;) {
         } else if (!ordinary) {
             fail(bundle.expected_a_b, nexttoken, '{', nexttoken.value);
         } else {
-            if (option.needcurly) {
-                warn(bundle.expected_a_b, nexttoken, '{', nexttoken.value);
-            }
+            warn(bundle.expected_a_b, nexttoken, '{', nexttoken.value);
             array = [statement()];
             array.disrupt = array[0].disrupt;
         }
@@ -4526,27 +4517,22 @@ loop:   for (;;) {
         discard();
         spaces(this, t);
         no_space();
-        if (option.varsattop && nexttoken.id === 'var') {
+        if (nexttoken.id === 'var') {
             fail(bundle.move_var);
         }
         edge();
-        if (peek(nexttoken.id === 'var' ? 1 : 0).id === 'in') {
-            if (nexttoken.id === 'var') {
-                advance('var');
-                token.fud();
-            } else {
-                v = nexttoken;
-                switch (funct[v.value]) {
-                case 'unused':
-                    funct[v.value] = 'var';
-                    break;
-                case 'var':
-                    break;
-                default:
-                    warn(bundle.bad_in_a, v);
-                }
-                advance();
+        if (peek(0).id === 'in') {
+            v = nexttoken;
+            switch (funct[v.value]) {
+            case 'unused':
+                funct[v.value] = 'var';
+                break;
+            case 'var':
+                break;
+            default:
+                warn(bundle.bad_in_a, v);
             }
+            advance();
             i = nexttoken;
             advance('in');
             i.first = v;
@@ -4561,19 +4547,14 @@ loop:   for (;;) {
             }
         } else {
             if (nexttoken.id !== ';') {
-                if (nexttoken.id === 'var') {
-                    advance('var');
-                    token.fud();
-                } else {
-                    edge();
-                    this.first = [];
-                    for (;;) {
-                        this.first.push(expression(0, 'for'));
-                        if (nexttoken.id !== ',') {
-                            break;
-                        }
-                        comma();
+                edge();
+                this.first = [];
+                for (;;) {
+                    this.first.push(expression(0, 'for'));
+                    if (nexttoken.id !== ',') {
+                        break;
                     }
+                    comma();
                 }
             }
             semicolon();
