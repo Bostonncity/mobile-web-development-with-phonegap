c9657aadfba5dbb849ba7fccede4e3743a3329aa
paul.beusterien@gmail.com <paul.beusterien@gmail.com@998cb456-c317-bc3b-214f-b0c30c965280>
2011-02-19 05:29:28 +0000

    Create jslintMirror branch to track JSLint repository

---
+++ branches/jslintMirror/com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/JSLintLog.java
@@ -0,0 +1,38 @@
+package com.googlecode.jslint4java.eclipse;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+public class JSLintLog {
+
+    // TODO How to get debug logging?
+
+    /**
+     * Format and log a message.
+     */
+    public static void info(String message, Object... args) {
+        message = String.format(message, args);
+        log(IStatus.INFO, IStatus.OK, message, null);
+    }
+
+    public static void error(Throwable t) {
+        log(IStatus.ERROR, IStatus.OK, "Unexpected exception", t);
+    }
+
+    public static void error(String message, Throwable t) {
+        log(IStatus.ERROR, IStatus.OK, message, t);
+    }
+
+    public static void log(int severity, int code, String message, Throwable t) {
+        log(createStatus(severity, code, message, t));
+    }
+
+    public static IStatus createStatus(int severity, int code, String message,
+            Throwable t) {
+        return new Status(severity, JSLintPlugin.PLUGIN_ID, code, message, t);
+    }
+
+    public static void log(IStatus status) {
+        JSLintPlugin.getDefault().getLog().log(status);
+    }
+}
