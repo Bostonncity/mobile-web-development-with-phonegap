a65f539d95fba2051b04289fd7a1031406e6b6b1
paul.beusterien@gmail.com <paul.beusterien@gmail.com@998cb456-c317-bc3b-214f-b0c30c965280>
2011-11-01 20:28:50 +0000

    Tag 1.2.4 before moving to 1.2.5

---
+++ tags/r1.2.4/com.googlecode.jslint4java.eclipse/src/com/googlecode/jslint4java/eclipse/builder/JSLintProvider.java
@@ -0,0 +1,65 @@
+package com.googlecode.jslint4java.eclipse.builder;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+
+import com.googlecode.jslint4java.JSLint;
+import com.googlecode.jslint4java.JSLintBuilder;
+import com.googlecode.jslint4java.Option;
+import com.googlecode.jslint4java.eclipse.JSLintLog;
+import com.googlecode.jslint4java.eclipse.JSLintPlugin;
+
+/**
+ * Provide a fully configured instance of {@link JSLint} on demand.
+ */
+public class JSLintProvider {
+
+    private final JSLintBuilder builder = new JSLintBuilder();
+
+    private JSLint jsLint;
+
+    /**
+     * Set up a listener for preference changes. This will ensure that the instance of
+     * {@link JSLint} that we have is kept in sync with the users choices. We do this by ensuring
+     * that a new JSLint will be created and configured on the next request.
+     */
+    public void init() {
+        IEclipsePreferences x = new InstanceScope().getNode(JSLintPlugin.PLUGIN_ID);
+        x.addPreferenceChangeListener(new IPreferenceChangeListener() {
+            public void preferenceChange(PreferenceChangeEvent ev) {
+                jsLint = null;
+                JSLintLog.info("pref %s changed; nulling jsLint", ev.getKey());
+            }
+        });
+    }
+
+    /**
+     * Return a fully configured instance of JSLint. This should not be cached; each use should call
+     * this method.
+     */
+    public JSLint getJsLint() {
+        if (jsLint == null) {
+            // TODO: Allow for non-default versions of fulljslint.js.
+            jsLint = builder.fromDefault();
+            configure();
+        }
+        return jsLint;
+    }
+
+    /** Set up the current instance of JSLint using the current preferences. */
+    public void configure() {
+        JSLint lint = getJsLint();
+        lint.resetOptions();
+        IPreferencesService prefs = Platform.getPreferencesService();
+        for (Option o : Option.values()) {
+            String value = prefs.getString(JSLintPlugin.PLUGIN_ID, o.getLowerName(), null, null);
+            if (value != null) {
+                lint.addOption(o, value);
+            }
+        }
+    }
+}
---
+++ tags/r1.2.4/com.mds.apg/src/com/mds/apg/wizards/FileCopy.java
@@ -0,0 +1,201 @@
+// thanks to http://www.java2s.com/Code/Java/File-Input-Output/CopyfilesusingJavaIOAPI.htm
+
+package com.mds.apg.wizards;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileCopy {
+
+    public static void recursiveCopy(String fromFileName, String toFileName) throws IOException {
+        copy (fromFileName, toFileName, true, false, null, false);
+    }
+    
+    public static void recursiveCopySkipSuffix(String fromFileName, String toFileName, String skip) throws IOException {
+        copy (fromFileName, toFileName, true, false, skip, false);
+    }
+
+    public static void recursiveForceCopy(String fromFileName, String toFileName) throws IOException {
+        copy (fromFileName, toFileName, true, true, null, false);
+    }
+
+    public static void forceCopy(String fromFileName, String toFileName) throws IOException{
+        copy (fromFileName, toFileName, false, true, null, false);
+    }    
+
+    public static void copy(String fromFileName, String toFileName) throws IOException{
+        copy (fromFileName, toFileName, false, false, null, false);
+    }
+
+    public static void copyDontOverwrite(String fromFileName, String toFileName) throws IOException{
+        copy (fromFileName, toFileName, false, false, null, true);      
+    }
+
+    private static void copy(String fromFileName, String toFileName, boolean isRecursive, 
+            boolean force, String skipSuffix, boolean dontOverwrite)   throws IOException {
+        
+        if (skipSuffix != null) {
+            int dotSpot = fromFileName.lastIndexOf('.');
+            if (dotSpot > 0 && fromFileName.substring(dotSpot).equals(skipSuffix)) {
+                return;
+            }
+        }
+        
+        File fromFile = new File(fromFileName);
+        File toFile = new File(toFileName);
+
+        if (!fromFile.exists())
+            throw new IOException("FileCopy: " + "no such source file: "
+                    + fromFileName);
+
+        if (isRecursive && fromFile.isDirectory()) {
+            if (toFile.exists()) {
+                if (!toFile.isDirectory()) {
+                    throw new IOException("FileCopy: " + "Cannot copy directory to non-directory: "
+                            + toFileName);
+                }
+            } else { // create the directory
+                toFile = new File(toFileName);
+                if (!toFile.mkdir()) {
+                    throw new IOException("FileCopy: " + "directory Creation Failed: "
+                            + toFileName);
+                }
+            }                
+            String fList[] = fromFile.list();
+            for (String s : fList) {
+                copy(fromFileName + "/" + s, toFileName + "/" + s, true, force, skipSuffix, dontOverwrite);
+            }
+            return;
+        }
+
+        if (!fromFile.isFile())
+            throw new IOException("FileCopy: " + "can't copy directory: "
+                    + fromFileName);
+        if (!fromFile.canRead())
+            throw new IOException("FileCopy: " + "source file is unreadable: "
+                    + fromFileName);
+
+        if (toFile.isDirectory()) 
+            toFile = new File(toFile, fromFile.getName());
+
+        if (toFile.exists()) {
+            if (!toFile.canWrite())
+                throw new IOException("FileCopy: "
+                        + "destination file is unwriteable: " + toFileName);
+            if (dontOverwrite) {
+                return;
+            }
+            if (!force) {
+                throw new IOException("FileCopy: "
+                        + "trying to overwrite an existing file" + toFileName);
+            }
+        } else {
+            String parent = toFile.getParent();
+            if (parent == null)
+                parent = System.getProperty("user.dir");
+            File dir = new File(parent);
+            if (!dir.exists())
+                throw new IOException("FileCopy: "
+                        + "destination directory doesn't exist: " + parent);
+            if (dir.isFile())
+                throw new IOException("FileCopy: "
+                        + "destination is not a directory: " + parent);
+            if (!dir.canWrite())
+                throw new IOException("FileCopy: "
+                        + "destination directory is unwriteable: " + parent);
+        }
+        coreStreamCopy(new FileInputStream(fromFile), toFile);
+    }
+    
+    public static void coreStreamCopy(InputStream from, File toFile) throws IOException {
+        FileOutputStream to = null;
+        try {
+            to = new FileOutputStream(toFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = from.read(buffer)) != -1)
+                to.write(buffer, 0, bytesRead); // write
+        } finally {
+            if (from != null)
+                try {
+                    from.close();
+                } catch (IOException e) {
+                    ;
+                }
+                if (to != null)
+                    try {
+                        to.close();
+                    } catch (IOException e) {
+                        ;
+                    }
+        }
+    }
+    
+    
+    public static void createPhonegapJs(String fromDirName, String toFileName)
+    throws IOException {
+        File fromFile = new File(fromDirName);
+        File toFile = new File(toFileName);
+
+        if (!fromFile.exists()) {
+            throw new IOException("createPhonegapJs: " + "no such source file: "
+                    + fromDirName);
+        }
+        FileOutputStream to = new FileOutputStream(toFile);
+        try {
+            to = new FileOutputStream(toFile);
+            to = appendStream(new File(fromDirName + "/" + "phonegap.js.base"), to);
+
+            String fList[] = fromFile.list();
+            for (String s : fList) {   // append the .js files
+                int i = s.lastIndexOf(".js");
+                if (i > 0 && i == s.length() - 3) {
+                    to = appendStream(new File(fromDirName + "/" + s), to);
+                }
+            }
+        } finally {
+            if (to != null) {
+                try {
+                    to.close();
+                } catch (IOException e) {
+                    ;
+                }
+            }
+        }
+    }
+    
+    public static boolean exists(String fileName) {
+        File file = new File(fileName);
+        return file.exists();
+    }
+    
+    private static FileOutputStream appendStream(File fromFile, FileOutputStream to) throws IOException {
+        
+        FileInputStream from = null;
+        try {
+            from = new FileInputStream(fromFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = from.read(buffer)) != -1)
+                to.write(buffer, 0, bytesRead); // write
+        } finally {
+            if (from != null) {
+                try {
+                    from.close();
+                } catch (IOException e) {
+                    ;
+                }
+            }
+        }
+        return to;
+    }
+}
+
+
+
+
